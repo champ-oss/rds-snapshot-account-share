7e2e77f723a81eef8e1eb507aaee476e4369d5f7
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-19 14:54:24 -0500

    python lambda snapshot account function

---
+++ README.md
@@ -0,0 +1,22 @@
+# rds-snapshot-account-share
+
+Summary: Lambda python function copy latest snapshot from rds db and share with another aws account
+
+![ci](https://github.com/conventional-changelog/standard-version/workflows/ci/badge.svg)
+[![version](https://img.shields.io/badge/version-1.x-yellow.svg)](https://semver.org)
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Project Status](#project-status)
+
+## General Information
+- Lambda python function that copies latest shapshot from db and shares with another aws account
+
+## Technologies Used
+- python 
+- Lambda
+- github actions
+
+## Project Status
+Project is: _in_progress_
--- rds_snapshot_account_share.py
+++ rds_snapshot_account_share.py
@@ -2,49 +2,87 @@
 
 import boto3
 import os
+import logging
+import sys
+
+from operator import itemgetter
+
 
 rds = boto3.client('rds')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def get_latest_snapshot(db_source):
-    source_snapshot = rds.describe_db_snapshots(
-        DBInstanceIdentifier=db_source,
-        SnapshotType='automated',
-        MaxRecords=1)
-    print(source_snapshot)
-    return source_snapshot
+
+    logger.info(f"get latest snapshot of: {db_source}")
+    response = rds.describe_db_snapshots(DBInstanceIdentifier=db_source, SnapshotType='automated')
+
+    sorted_keys = sorted(response['DBSnapshots'], key=itemgetter('SnapshotCreateTime'), reverse=True)
+    snapshot_id = sorted_keys[0]['DBSnapshotIdentifier']
+
+    logger.info(f"returned latest snapshot: {snapshot_id}")
+    return snapshot_id
+
+def snapshot_exist(snapshot):
+    try:
+        snapshot_response = rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot)
+        return(snapshot_response)
+    except:
+        return None
 
 def copy_snapshot(snapshot):
-    rds_prefix, target_snapshot = snapshot.split(":")
-    print(rds_prefix)
-    rds.copy_db_snapshot(
-        SourceDBSnapshotIdentifier=snapshot,
-        TargetDBSnapshotIdentifier=target_snapshot)
+
+    rds_prefix, rds_snapshot = snapshot.split(":")
+    target_snapshot = rds_prefix + "-" + rds_snapshot
+    check_snapshot_exist = snapshot_exist(target_snapshot)
+    if check_snapshot_exist == None:
+        print("copy doesn't exist, exec copy")
+        rds.copy_db_snapshot(
+            SourceDBSnapshotIdentifier=snapshot,
+            TargetDBSnapshotIdentifier=target_snapshot)
+    else:
+        sys.exit("copy exist, clean exit")
+
     return target_snapshot
 
 def wait_target_snapshot(target_snapshot):
+    print("waiting for snapshot to copy")
+    logger.info(f"waiting for snapshot to copy")
     waiter = rds.get_waiter('db_snapshot_available')
+    # max lambda timeout is 15 minutes
     waiter.wait(
-        DBInstanceIdentifier=db_identifier,
         DBSnapshotIdentifier=target_snapshot,
-        WaiterConfig={'Delay': 15, 'MaxAttempts': 10}
+        WaiterConfig={'Delay': 30, 'MaxAttempts': 26}
     )
-
+    print("snapshot copy complete")
+    logger.info(f"snapshot copy complete")
 def share_snapshot(target_snapshot, aws_shared_account):
-    rds.modify_db_snapshot_attribute(
+    logger.info(f"share snapshot to another account")
+    response = rds.modify_db_snapshot_attribute(
         DBSnapshotIdentifier=target_snapshot,
         AttributeName='restore',
         ValuesToAdd=[
             aws_shared_account,
         ],
     )
+    logger.info(f"share snapshot to another account complete")
 
-def main():
+def lambda_handler(event, context):
+
+    # env variables requirement
     db_source = os.environ['DB_SOURCE']
     aws_shared_account = os.environ['AWS_SHARED_ACCOUNT']
+
+    # get latest snapshot from db
     get_snapshot = get_latest_snapshot(db_source)
+
+    # check if snapshot already copied before copying latest automated snapshot from db
     target_snapshot = copy_snapshot(get_snapshot)
+
+    # wait for snapshot to be copied with retry logic
     wait_target_snapshot(target_snapshot)
+
+    # share snapshot with another aws account
     share_snapshot(target_snapshot,aws_shared_account)
 
-main()
