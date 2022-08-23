a0a9dac67d544a7d004e622de7ff4b6d0f5b9486
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-23 13:35:51 -0500

    updated enhancements for rds snapshot account share

--- rds_snapshot_account_share.py
+++ rds_snapshot_account_share.py
@@ -3,86 +3,141 @@
 import boto3
 import os
 import logging
-import sys
+import time
+import csv
 
 from operator import itemgetter
-
 
 rds = boto3.client('rds')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def get_db_instances(writeFilePath):
+    logger.info("get list of db instances")
+    with open(writeFilePath, 'w') as writeFile:
+        response = rds.describe_db_instances()
+        for i in response['DBInstances']:
+            db_instance_name = i['DBInstanceIdentifier']
+            writeFile.write(db_instance_name)
+            writeFile.write("\n")
+
+
+def get_snapshot_tags(arn):
+    logger.info("checking tag value")
+    tags = rds.list_tags_for_resource(ResourceName=arn)['TagList']
+    if 'copy-in-progress' in [tag['Value'] for tag in tags]:
+        return "copy-in-progress"
+    elif 'copy-complete' in [tag['Value'] for tag in tags]:
+        return "copy-complete"
+    else:
+        return None
+
+
+def update_snapshot_tags(arn, tag_value):
+    logger.info("update snapshot tag value")
+    rds.add_tags_to_resource(
+        ResourceName=arn,
+        Tags=[
+            {
+                'Key': 'rds-util-status',
+                'Value': tag_value
+            },
+        ]
+    )
+    logger.info("update to snapshot tag value complete")
+
+
 def get_latest_snapshot(db_source):
-
     logger.info(f"get latest snapshot of: {db_source}")
-    response = rds.describe_db_snapshots(DBInstanceIdentifier=db_source, SnapshotType='automated')
-
+    response = rds.describe_db_snapshots(DBInstanceIdentifier=db_source)
     sorted_keys = sorted(response['DBSnapshots'], key=itemgetter('SnapshotCreateTime'), reverse=True)
     snapshot_id = sorted_keys[0]['DBSnapshotIdentifier']
+    snapshot_arn = sorted_keys[0]['DBSnapshotArn']
 
     logger.info(f"returned latest snapshot: {snapshot_id}")
-    return snapshot_id
+    logger.info(f"returned latest snapshot arn: {snapshot_arn}")
+    return snapshot_id, snapshot_arn
+
 
 def snapshot_exist(snapshot):
     try:
-        snapshot_response = rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot)
-        return(snapshot_response)
+        logger.info("checking to see if snapshot exist or not")
+        response = rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot)
+        logger.info(f"returned latest snapshot: {response}")
+        return response
     except:
+        logger.info("snapshot does not exist")
         return None
 
-def copy_snapshot(snapshot):
 
-    rds_prefix, rds_snapshot = snapshot.split(":")
-    target_snapshot = rds_prefix + "-" + rds_snapshot
-    check_snapshot_exist = snapshot_exist(target_snapshot)
-    if check_snapshot_exist == None:
-        print("copy doesn't exist, exec copy")
-        rds.copy_db_snapshot(
-            SourceDBSnapshotIdentifier=snapshot,
-            TargetDBSnapshotIdentifier=target_snapshot)
+def delete_latest_snapshot(latest_snapshot):
+    logger.info("delete latest snapshot")
+    check_snapshot_exist = snapshot_exist(latest_snapshot)
+    if check_snapshot_exist is None:
+        logger.info("latest doesnt exist for delete")
+        return
     else:
-        sys.exit("copy exist, clean exit")
+        rds.delete_db_snapshot(
+            DBSnapshotIdentifier=latest_snapshot,
+        )
+        time.sleep(5)
+        logger.info("deleted latest snapshot complete")
 
-    return target_snapshot
 
-def wait_target_snapshot(target_snapshot):
-    print("waiting for snapshot to copy")
-    logger.info(f"waiting for snapshot to copy")
-    waiter = rds.get_waiter('db_snapshot_available')
-    # max lambda timeout is 15 minutes
-    waiter.wait(
-        DBSnapshotIdentifier=target_snapshot,
-        WaiterConfig={'Delay': 30, 'MaxAttempts': 26}
+def copy_snapshot(source_snapshot, target_snapshot):
+    logger.info("copy snapshot")
+
+    rds.copy_db_snapshot(
+        SourceDBSnapshotIdentifier=source_snapshot,
+        TargetDBSnapshotIdentifier=target_snapshot,
+        CopyTags=True
     )
-    print("snapshot copy complete")
-    logger.info(f"snapshot copy complete")
+    logger.info("copy snapshot complete")
+
+
 def share_snapshot(target_snapshot, aws_shared_account):
-    logger.info(f"share snapshot to another account")
-    response = rds.modify_db_snapshot_attribute(
+    logger.info("share snapshot to another account")
+    rds.modify_db_snapshot_attribute(
         DBSnapshotIdentifier=target_snapshot,
         AttributeName='restore',
         ValuesToAdd=[
             aws_shared_account,
         ],
     )
-    logger.info(f"share snapshot to another account complete")
+    logger.info("share snapshot to another account complete")
+
 
 def lambda_handler(event, context):
-
-    # env variables requirement
-    db_source = os.environ['DB_SOURCE']
+    # env variables requirement, used to share snapshot with another account
     aws_shared_account = os.environ['AWS_SHARED_ACCOUNT']
 
-    # get latest snapshot from db
-    get_snapshot = get_latest_snapshot(db_source)
+    # get_db_instances("dbIdentifierList.csv")
+    with open("dbIdentifierList.csv") as readFile:
+        db_sources = csv.reader(readFile)
+        for db_source in db_sources:
+            # get latest snapshot from db
+            get_snapshot, snapshot_arn = get_latest_snapshot(db_source[0])
 
-    # check if snapshot already copied before copying latest automated snapshot from db
-    target_snapshot = copy_snapshot(get_snapshot)
+            # check current tag on snapshot
+            check_tag_value = get_snapshot_tags(snapshot_arn)
+            print(check_tag_value)
+            # latest tag has been copied but not shared
+            if check_tag_value == "copy-in-progress":
+                # update tag before sharing with another account
+                update_snapshot_tags(snapshot_arn, "copy-complete")
+                # sharing snapshot with another account
+                share_snapshot(get_snapshot, aws_shared_account)
 
-    # wait for snapshot to be copied with retry logic
-    wait_target_snapshot(target_snapshot)
+            # latest tag is copied and shared, return and
+            elif check_tag_value == "copy-complete":
+                logger.info("nothing to do, latest snapshot copied")
 
-    # share snapshot with another aws account
-    share_snapshot(target_snapshot,aws_shared_account)
+            # this is an automated tag, not tagged and needs to be copied
+            elif check_tag_value is None:
+                update_snapshot_tags(snapshot_arn, "copy-in-progress")
+                delete_latest_snapshot(db_source[0])
+                copy_snapshot(get_snapshot, db_source[0])
 
+            else:
+                logger.warning("latest tag check failed")
